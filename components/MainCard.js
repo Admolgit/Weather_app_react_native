94dbe3ce412bc88bb15071eeae0269e1f8326e68
Admolgit <zealous890@gmail.com>
2023-05-26 00:14:25 +0100

    updated weather app

---
+++ components/MainCard.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View } from 'react-native';
+// Icons
+import { Feather, Fontisto, FontAwesome } from '@expo/vector-icons';
+
+const MainCard = (props) => {
+  const Icon = () => {
+    if (props.icon === 'morning') {
+      return (
+        <Feather name="sun" style={styles.cardIcon} size={40} color="orange" />
+      );
+    }
+    if (props.icon === 'afternoon') {
+      return (
+        <Fontisto
+          name="day-cloudy"
+          style={styles.cardIcon}
+          size={40}
+          color="orange"
+        />
+      );
+    }
+    if (props.icon === 'night') {
+      return (
+        <FontAwesome
+          name="moon-o"
+          style={styles.cardIcon}
+          size={40}
+          color="orange"
+        />
+      );
+    }
+  };
+
+  const styles = StyleSheet.create({
+    card: {
+      backgroundColor: props.backgroundColor,
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 10,
+      width: 110,
+      height: 210,
+      borderRadius: 10,
+    },
+    refreshButton: {
+      position: 'absolute',
+      margin: 30,
+      alignSelf: 'flex-start',
+    },
+    cardText: {
+      color: 'white',
+      margin: 15,
+      fontSize: 20,
+    },
+    cardIcon: {
+      color: 'white',
+      margin: 15,
+    },
+  });
+
+  return (
+    <View style={styles.card}>
+      <Text style={styles.cardText}>{props.title}</Text>
+      <Icon></Icon>
+      <Text style={styles.cardText}>{props.temperature}</Text>
+    </View>
+  );
+};
+
+export default MainCard;
